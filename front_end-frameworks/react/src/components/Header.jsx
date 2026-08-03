e76ac82175ec919f6a55946c8128dadb6c90824f
Clement Callejon <clement.callejon@tutanota.com>
2026-08-03 17:17:56 +0200

    feat: improve legibility of Header component

--- front_end-frameworks/react/src/components/Header.jsx
+++ front_end-frameworks/react/src/components/Header.jsx
@@ -10,12 +10,16 @@ export default function Header() {
 
   return (
     <header className="flex justify-between p-5 md:px-15 bg-slate-950 fixed w-full">
+
+      <!-- Logo -->
       <div className="flex items-center gap-2">
         <div className="p-2 bg-violet-500 rounded-xl shadow-lg shadow-violet-500/40">
           <BrainCircuit className="stroke-violet-300"/>
         </div>
         <span className="text-slate-50 font-bold">Agentic AI</span>
       </div>
+
+      <!-- Nav buttons -->
       <nav className="flex gap-4 text-slate-500 items-center">
         <div className="hidden md:flex">
           {navLinks.map(({ href, label }) => (
@@ -28,6 +32,7 @@ export default function Header() {
           Enroll now
         </a>
       </nav>
+
     </header>
   );
 }
