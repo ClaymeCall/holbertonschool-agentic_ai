a9426c1668467ec11fada3c02d41986bc0d5729d
Clement Callejon <clement.callejon@tutanota.com>
2026-08-03 17:19:25 +0200

    fix: remove comments

--- front_end-frameworks/react/src/components/Header.jsx
+++ front_end-frameworks/react/src/components/Header.jsx
@@ -11,7 +11,6 @@ export default function Header() {
   return (
     <header className="flex justify-between p-5 md:px-15 bg-slate-950 fixed w-full">
 
-      <!-- Logo -->
       <div className="flex items-center gap-2">
         <div className="p-2 bg-violet-500 rounded-xl shadow-lg shadow-violet-500/40">
           <BrainCircuit className="stroke-violet-300"/>
@@ -19,7 +18,6 @@ export default function Header() {
         <span className="text-slate-50 font-bold">Agentic AI</span>
       </div>
 
-      <!-- Nav buttons -->
       <nav className="flex gap-4 text-slate-500 items-center">
         <div className="hidden md:flex">
           {navLinks.map(({ href, label }) => (
