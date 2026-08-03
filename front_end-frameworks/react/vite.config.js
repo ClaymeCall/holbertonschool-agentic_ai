9376fdaa118208600c8ca98df38f4785ab99eefb
Clement Callejon <clement.callejon@tutanota.com>
2026-08-03 11:10:32 +0200

    feat: add repo root gitignore for devenv files

--- front_end-frameworks/react/vite.config.js
+++ front_end-frameworks/react/vite.config.js
@@ -4,12 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [
     react(),
     tailwindcss(),
   ],
   server: {
-    host: true,
+    host: "0.0.0.0",
     port: 3000,
   },
 })
