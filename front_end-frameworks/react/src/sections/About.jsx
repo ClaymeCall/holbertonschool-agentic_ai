be26f29ddec331b363829e2a3e9f28ed5d88ef10
Clement Callejon <clement.callejon@tutanota.com>
2026-08-04 16:19:37 +0200

    feat: implement ai comparison card

--- front_end-frameworks/react/src/sections/About.jsx
+++ front_end-frameworks/react/src/sections/About.jsx
@@ -1,11 +1,12 @@
-import {Dot} from 'lucide-react';
 import Eyebrow from '../components/Eyebrow.jsx'
 import steps from '../data/steps.js'
 
 export default function About() {
   return (
     <section id="about-section" className="flex flex-col items-center py-24">
+
       <Eyebrow>What is Agentic AI?</Eyebrow>
+
       <hgroup className="mb-10 flex flex-col items-center">
         <h2 className="mb-10 text-center text-4xl md:text-5xl font-black tracking-tight leading-none">
           <p className="text-slate-50">
@@ -20,23 +21,46 @@ export default function About() {
         </p>
       </hgroup>
 
-      <div className="flex flex-col">
-        {steps.map((step) => (
-          <div key={step.number} className="flex gap-8 pb-5">
-            <div className="flex justify-center items-center bg-violet-500 size-8 rounded-full text-xs font-black">
-              {step.number}
-            </div>
-            <div className="pt-1">
-              <h3 className="font-bold text-slate-50">
-                {step.title}
-              </h3>
-              <p className="text-slate-300">
-                {step.description}
-              </p>
-            </div>
-          </div>
-        ))}
+      <div className="flex flex-col items-center w-full gap-10 sm:flex-row">
+
+        <div className="bg-slate-950 mb-10 p-7 flex-shrink-3 h-fit border border-slate-500 rounded-3xl">
+          <dl className="w-full">
+            <dt className="pb-2 text-lg font-bold">
+              Traditional AI
+            </dt>
+            <dd className="text-slate-500">
+              Responds to direct instructions, generates content, answers questions, or analyzes information within limited interaction.
+            </dd>
+            <hr className="bg-slate-500 my-5" />
+            <dt className="text-violet-300 font-bold">
+              Agentic AI
+            </dt>
+            <dd className="text-slate-500">
+              Understands a goal, chooses actions, uses external tools, follows a plan, and adjusts its behaviour based on feedback.
+            </dd>
+          </dl>
+        </div>
+
+        <ol className="flex flex-col flex-shrink-2">
+          {steps.map((step) => (
+            <li key={step.number} className="flex gap-8 pb-5">
+              <div className="flex justify-center items-center bg-violet-500 flex-shrink-0 size-8 rounded-full text-xs font-black">
+                {step.number}
+              </div>
+              <div className="pt-1">
+                <h3 className="font-bold text-slate-50">
+                  {step.title}
+                </h3>
+                <p className="text-slate-300">
+                  {step.description}
+                </p>
+              </div>
+            </li>
+          ))}
+        </ol>
+
       </div>
+
     </section>
   );
 }
