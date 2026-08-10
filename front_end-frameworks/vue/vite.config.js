e8f00c69888aa566cb6dca06ff43bc5eb3be4f7a
Clement Callejon <clement.callejon@tutanota.com>
2026-08-10 20:11:14 +0200

    chore: move devenv to repo root

---
+++ front_end-frameworks/vue/vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: "/holbertonschool-agentic_ai/front_end-frameworks/vue/",
+  plugins: [
+    vue(),
+    tailwindcss(),
+  ],
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
+    emptyOutDir: true,
+  },
+})
