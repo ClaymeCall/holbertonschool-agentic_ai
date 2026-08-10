4000b94dd532d562638cb212418abae65806a155
Clement Callejon <clement.callejon@tutanota.com>
2026-08-10 18:59:42 +0200

    feat: change react deployment to have dedicated url

--- front_end-frameworks/react/src/components/cards/StatCard.jsx
+++ front_end-frameworks/react/src/components/cards/StatCard.jsx
@@ -1,6 +1,6 @@
 export default function StatCard({ stat, value }) {
   return (
-    <div className="rounded-xl border border-slate-800 bg-slate-950 p-6 shadow-xl shadow-slate-950/40">
+    <div className="flex flex-col-reverse rounded-xl border border-slate-800 bg-slate-950 p-6 shadow-xl shadow-slate-950/40">
       <dt key={stat} className="text-center text-xs text-slate-500">
         {stat}
       </dt>
--- front_end-frameworks/react/vite.config.js
+++ front_end-frameworks/react/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
+  base: '/holbertonschool-agentic_ai/front_end-frameworks/react/',
   plugins: [
     react(),
     tailwindcss(),
